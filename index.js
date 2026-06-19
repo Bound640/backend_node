7deacdc7714628584685c1d22f6c9668f4e1cda3
Bound640 <boundiandao3@gmail.com>
2026-06-19 12:14:41 +0200

    commit

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ connectBD();
 app.use(express.json());
 app.use(cors({ origin: [
      "http://localhost:5173",
-     ""
+     "https://front-node-syta.vercel.app"
 ]}));
 const PORT = process.env.PORT;
 app.listen( PORT , () => {
